2513283e75ac9300966976a071b4846cd816568b
SteveSande <stevenandrewsande@gmail.com>
2023-08-14 22:28:42 -0400

    added tailwind

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,7 @@
 export default function Header () {
     return (
         <>      
-            <h1>Find Your Dream Dog</h1>
+            <h1 className='font-bold'>Find Your Dream Dog</h1>
             <p>Pick the dog that you connect with the most until you have seen them all.</p>
         </>
     )
