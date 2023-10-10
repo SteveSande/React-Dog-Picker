3ef6f85fdf64473125c03997a1b51c4a16c31ba7
SteveSande <stevenandrewsande@gmail.com>
2023-10-09 21:18:14 -0400

    button background color toggles for dark mode

--- src/components/DarkToggle.tsx
+++ src/components/DarkToggle.tsx
@@ -1,11 +1,37 @@
 import { Button } from "ariakit";
+import { useAtom, useSetAtom } from "jotai";
+import { darkModeAtom } from "../utils/atom.ts";
+import { useLocalStorage } from "usehooks-ts";
+import { useEffect, useState } from "react";
 
 export default function DarkToggle() {
+  const [darkMode, setDarkMode] = useAtom(darkModeAtom);
+  const [darkModeLS, setDarkModeLS] = useLocalStorage<boolean>(
+    "darkMode",
+    false
+  );
+  const [bgColor, setBgColor] = useState<String>("bg-black");
+
+  const toggleDarkMode = () => {
+    setDarkMode((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (darkMode) {
+      setBgColor("bg-white");
+    } else {
+      setBgColor("bg-black");
+    }
+
+    setDarkModeLS(darkMode);
+  }, [darkMode]);
+
   return (
     <div id="right-side" className="w-1/5 flex justify-end items-center">
       <Button
         id="dark-toggle"
-        className="bg-black mr-10 h-fit font-bold text-white rounded-lg p-1"
+        className={`${bgColor} mr-10 h-fit font-bold text-white rounded-lg p-1`}
+        onClick={toggleDarkMode}
       >
         Dark
       </Button>
