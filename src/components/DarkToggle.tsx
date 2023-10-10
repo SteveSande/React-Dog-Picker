e3d05501694e609f81c714763831edf253afbf07
SteveSande <stevenandrewsande@gmail.com>
2023-10-09 21:54:26 -0400

    button text updating and weird build bug

--- src/components/DarkToggle.tsx
+++ src/components/DarkToggle.tsx
@@ -7,20 +7,23 @@ export default function DarkToggle() {
   const [darkMode, setDarkMode] = useAtom(darkModeAtom);
   const [bgColor, setBgColor] = useState<String>("bg-black");
   const [textColor, setTextColor] = useState<String>("text-white");
+  const [text, setText] = useState<String>("Dark");
 
   const toggleDarkMode = () => {
     setDarkMode((prev) => !prev);
   };
 
   useEffect(() => {
-    localStorage.setItem("darkMode", darkMode.toString());
+    console.log("trigger");
 
     if (darkMode) {
       setBgColor("bg-white");
       setTextColor("text-black");
+      setText("Light");
     } else {
       setBgColor("bg-black");
       setTextColor("text-white");
+      setText("Dark");
     }
   }, [darkMode]);
 
@@ -28,10 +31,10 @@ export default function DarkToggle() {
     <div id="right-side" className="w-1/5 flex justify-end items-center">
       <Button
         id="dark-toggle"
-        className={`${bgColor} ${textColor} mr-10 h-fit font-bold text-white rounded-lg p-1`}
+        className={`${bgColor} ${textColor} mr-10 h-fit font-bold text-white rounded-lg p-1 w-[55px]`}
         onClick={toggleDarkMode}
       >
-        Dark
+        {text}
       </Button>
     </div>
   );
